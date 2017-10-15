58605929e3a239415c2a7ed3b27fe7d5ac8871ff
Marco Mendes <1111055@isep.ipp.pt>
2017-10-15 21:27:31 +0100

    act
    act

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,23 +1,6 @@
 <!--Footer-part-->
-
-<div class="row-fluid">
-    <div id="footer" class="span12"> 2017 &copy; ISEP/PESTI 2016/2017 Realizado por Marco Mendes </div>
-</div>
-
-<!--end-Footer-part-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
-{!!Html::script('js/jquery.min.js')!!}
-{!!Html::script('js/jquery.ui.custom.js')!!}
-{!!Html::script('js/jquery.dataTables.min.js')!!}
-
-{!!Html::script('js/bootstrap.min.js')!!}
-{!!Html::script('js/jquery.uniform.js')!!}
-{!!Html::script('js/select2.min.js')!!}
-{!!Html::script('js/matrix.tables.js')!!}
-{!!Html::script('js/matrix.charts.js')!!}
-{!!Html::script('js/matrix.js')!!}
-{!!Html::script('js/matrix.tables.js')!!}
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="footer-copyright">&copy; 2014 <a href="http://www.vactualart.com/portfolio-item/basica-a-free-html5-template/">Basica</a> Bootstrap HTML Template. By <a href="http://www.vactualart.com">Vactual Art</a>.</div>
+                    </div>
+                </div>
--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,28 +1,28 @@
 <head>
-    <title>Frota +</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/bootstrap-responsive.min.css')}}" />
 
-    <link rel="stylesheet" href="{{asset('css/css-style.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/css-media.css')}}" />
-    <link rel="stylesheet" href="{{asset('font-awesome/css/font-awesome.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/jquery.gritter.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/uniform.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/select2.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/jquery.dataTables.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/buttons.dataTables.min.css')}}" />
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- Isolated Version of Bootstrap, not needed if your site already uses Bootstrap -->
-    <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
+    <title>FeelBit - Digital Markting</title>
 
-    <!-- Bootstrap Date-Picker Plugin -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
 
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/main.css">
+    <link href="css/custom.css" rel="stylesheet">
+    
+    <script src="//use.edgefonts.net/bebas-neue.js"></script>
+
+    <!-- Custom Fonts & Icons -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600,800' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/icomoon-social.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    
+    <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    
 
 </head>
