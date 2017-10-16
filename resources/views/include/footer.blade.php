573fb6f4f72170739956e66bdfd47a59a7885969
1111055 <1111055@isep.ipp.pt>
2017-10-16 17:20:30 +0100

    up f
    up f

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -8,26 +8,23 @@
                 <div class="row">
                 
                     <div class="col-footer col-md-4 col-xs-6">
-                        <h3>Contact Us</h3>
+                        <h3>Contactos</h3>
                         <p class="contact-us-details">
-                            <b>Address:</b> 123 Downtown Avenue, Manhattan, New York, United States of America<br/>
-                            <b>Phone:</b> +1 123 45678910<br/>
-                            <b>Fax:</b> +1 123 45678910<br/>
-                            <b>Email:</b> <a href="mailto:info@yourcompanydomain.com">info@yourcompanydomain.com</a>
+                            <b>Morada:</b> Teste Morada 555, 8889-55 VNF<br/>
+                            <b>Telefone:</b> +1 123 45678910<br/>
+                            <b>Email:</b> <a href="mailto:geral@feelbit.pt">geral@feelbit.pt</a>
                         </p>
                     </div>              
                     <div class="col-footer col-md-4 col-xs-6">
-                        <h3>Our Social Networks</h3>
+                        <h3>Redes Sociais</h3>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.</p>
                         <div>
                             <img src="img/icons/facebook.png" width="32" alt="Facebook">
-                            <img src="img/icons/twitter.png" width="32" alt="Twitter">
                             <img src="img/icons/linkedin.png" width="32" alt="LinkedIn">
-                            <img src="img/icons/rss.png" width="32" alt="RSS Feed">
                         </div>
                     </div>
                     <div class="col-footer col-md-4 col-xs-6">
-                        <h3>About Our Company</h3>
+                        <h3>Sobre Nós</h3>
                             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci.</p>
                     </div>
 
@@ -37,6 +34,6 @@
         </div>
         <div class="row">
                     <div class="col-md-12">
-                        <div class="footer-copyright">&copy; 2017 <a href="http://www.vactualart.com">FeelBit - Digital Marketing</a>.</div>
+                        <div class="footer-copyright">&copy; 2017 <a href="http://www.feelbit.pt">FeelBit - Digital Marketing</a>.</div>
                     </div>
         </div>
