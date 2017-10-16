3e4deccb88f868569da9a71076f6b8e2de364c72
1111055 <1111055@isep.ipp.pt>
2017-10-16 11:39:04 +0100

    up footer
    up footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,6 +1,6 @@
 <!--Footer-part-->
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="footer-copyright">&copy; 2014 <a href="http://www.vactualart.com/portfolio-item/basica-a-free-html5-template/">Basica</a> Bootstrap HTML Template. By <a href="http://www.vactualart.com">Vactual Art</a>.</div>
+                        <div class="footer-copyright">&copy; 2017 <a href="http://www.vactualart.com">FeelBit - Digital Marketing</a>.</div>
                     </div>
                 </div>
